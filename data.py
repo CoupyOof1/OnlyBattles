fef2d2de043d0879aaeef8a465152cd26b365ce3
CoupyOof19 <culturedcoupy@gmail.com>
2025-06-22 19:45:20 +0100

    Main + Data py Files Added

---
+++ data.py
@@ -0,0 +1,4 @@
+import pygame    
+
+# Change for each update made (Michael I know you are reading this)
+UPDATE_LOG = '0.0.1'
---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+import random
+import math
+
+# Initialize Pygame
+pygame.init()
+pygame.display.set_caption('OnlyBattles'+UPDATE_LOG) # Display Game Name on Window
+
+#set framerate of the game
+clock = pygame.time.Clock()
+FPS = 60 
